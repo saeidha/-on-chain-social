f2a1395a2bada786319be59f05f06c36ef6252da
saeed221373 <saeed221373@googlemail.com>
2024-10-18 22:39:24 +0000

    add transaction count

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { Avatar, Name } from '@coinbase/onchainkit/identity';
 import { base } from 'viem/chains';
 import '@coinbase/onchainkit/styles.css';
 import SwapComponents from './Swap'
+import { useTransactionCount } from 'wagmi'
 
 function App() {
   const account = useAccount()
   const { connectors, connect, status, error } = useConnect()
   const { disconnect } = useDisconnect()
   const address = account.address
-
+  const result = useTransactionCount({address: address})
+  
   return (
     <>
       <div>
@@ -56,6 +58,7 @@ function App() {
         <div>{error?.message}</div>
       </div>
 
+      <p>tansaction count: {result.data}</p>
       <SwapComponents/> 
     </>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{ts,tsx}'],
+  darkMode: ['class'], 
+  safelist: ['dark'], 
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
+
